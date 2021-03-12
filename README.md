b6cac46108a5d2655945f2bc62d789f1c3e183cc
Bernard <bernardyeo001@gmail.com>
2021-03-12 23:42:50 +0800

    update Readme & add SQL case study

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SQL-Bank
-A collection of SQL test that I have encountered
+This repository host a collection of SQL test that I have encountered.
+While most of the time you'll be asked to write out the SQL query given a set of table schema, creating a mock up table in BigQuery will help a lot (if time allows for it) 
+
+### How to create a temporary SQL table in BigQuery
---
+++ WarnerMedia/SQL_WM_Q2_Solution.sql
@@ -0,0 +1,20 @@
+WITH 
+------------------------------------------------- group Cust_ID & Content_ID
+grouped_table AS (
+	SELECT
+		Cust_ID
+		,Content_ID
+		,SUM(Play_duration) AS Play_Duration_Grp
+	FROM tbl1
+	GROUP BY 1,2
+),
+
+------------------------------------------------- find acquisition channel time
+SELECT
+	t2.Acq_channel
+	,SUM(Play_Duration_Grp) AS Play_duration
+FROM grouped_table gt1
+INNER JOIN tbl2 t2
+	ON gt1.Cust_ID = t2.Advertising_id OR gt1.Cust_ID = t2.IDFA OR gt1.Cust_ID = t2.IDFV
+GROUP BY 1
+ORDER BY 2 DESC
