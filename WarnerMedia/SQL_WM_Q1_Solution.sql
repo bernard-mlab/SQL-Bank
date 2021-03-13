bf2397edf004fa8f1a360c3643d68be78a276df5
Bernard <bernardyeo001@gmail.com>
2021-03-13 16:42:49 +0800

    update tab as space

--- WarnerMedia/SQL_WM_Q1_Solution.sql
+++ WarnerMedia/SQL_WM_Q1_Solution.sql
@@ -1,28 +1,28 @@
 WITH 
 ------------------------------------------------- group Cust_ID & Content_ID
 grouped_table AS (
-	SELECT
-		*
-		,RANK() OVER(
-			PARTITION BY Cust_ID
-			ORDER BY Play_Duration_Grp ASC) AS durationRank
+  SELECT
+    *
+    ,RANK() OVER(
+      PARTITION BY Cust_ID
+      ORDER BY Play_Duration_Grp ASC) AS durationRank
 	
-	FROM (
-		SELECT
-			Cust_ID
-			,Content_ID
-			,SUM(Play_duration) AS Play_Duration_Grp
-		FROM tbl
-		GROUP BY 1,2
-	)
+  FROM (
+    SELECT
+      Cust_ID
+      ,Content_ID
+      ,SUM(Play_duration) AS Play_Duration_Grp
+    FROM tbl
+    GROUP BY 1,2
+  )
 )
 
 ------------------------------------------------- find least watched content
 SELECT 
-	Cust_ID
-	,Content_ID
-	,Play_Duration_Grp AS Play_duration
+  Cust_ID
+  ,Content_ID
+  ,Play_Duration_Grp AS Play_duration
 
 FROM grouped_table
 WHERE 1=1
-	AND durationRank = 1
+  AND durationRank = 1 
